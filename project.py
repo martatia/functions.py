22f93f8459f7f4d8daae02b3a47375eb84bb244c
Marta <marta.tiainen@gmail.com>
2020-09-25 17:25:57 +0300

    errors corrected

--- project.py
+++ project.py
@@ -2,7 +2,7 @@
 import math
 
 print("The simple calculator is only for two numbers")
-def summa(a:float,b:float):
+def addition(a:float,b:float):
     """[counts sum of two numbers]
 
     Args:
@@ -14,7 +14,7 @@ def summa(a:float,b:float):
     """
     return a+b
 
-def ero(a:float,b:float):
+def substraction(a:float,b:float):
     """[counts substraction of two numbers]
 
     Args:
@@ -26,7 +26,7 @@ def ero(a:float,b:float):
     """
     return a-b
 
-def tulo(a:float,b:float):
+def multiplication(a:float,b:float):
     """[return multiplication of two numbers]
 
     Args:
@@ -38,7 +38,7 @@ def tulo(a:float,b:float):
     """
     return a*b
 
-def jako(a:float,b:float):
+def division(a:float,b:float):
     """[returns division of two numbers]
 
     Args:
@@ -48,9 +48,12 @@ def jako(a:float,b:float):
     Returns:
         [type]: [division of number1 by number2]
     """
-    return a/b
+    try:
+        return a/b
+    except ZeroDivisionError:
+        print("Cannot divide by 0")
 
-def potenssi(a:float,b:float):
+def power(a:float,b:float):
     """[returns power]
 
     Args:
@@ -63,7 +66,7 @@ def potenssi(a:float,b:float):
     return a**b
 
 def root(a:float,b:float):
-    """[returns 2 roots of two different numbers]
+    """[returns 2 square roots of two different numbers]
 
     Args:
         a (float): [number1]
@@ -72,26 +75,39 @@ def root(a:float,b:float):
     Returns:
         [float]: [square root of number1 and square root of number2]
     """
-    return math.sqrt(a),"and",math.sqrt(b)
+    if a<0 and b>0:
+        print(f"The first number is bellow 0, the second square root is {math.sqrt(b)}")
+    elif a>0 and b<0:
+        print(f"The first square root is {math.sqrt(a)}, number is bellow 0")
+    else:
+        return f"{math.sqrt(a)} and {math.sqrt(b)}"
+
 
 try:
-    print("1=summa; 2=substraction; 3=multiplication; 4=division; 5=power(number1^number2); 6=square root of two numbers")
-    valinta=int(input("Please choose what would you like to count(1-6): "))
-    number1=float(input("Please enter the 1. number: "))
-    number2=float(input("Please enter the 2.number: "))
-    if valinta==1:
-        print(summa(number1,number2))
-    elif valinta==2:
-        print(ero(number1,number2))
-    elif valinta==3:
-        print(tulo(number1,number2))
-    elif valinta==4:
-        print(jako(number1,number2))
-    elif valinta==5:
-        print(potenssi(number1,number2))
-    elif valinta==6:
-        print(root(number1,number2))
-    else:
-        print("Wrong input, you can choose only between 1-6")
-except:
-    print("Wrong input, you can choose only between 1-6")
+    while True:
+        print("1=addition; 2=substraction; 3=multiplication; 4=division; 5=power(number1^number2); 6=square root of two numbers; 7=exit")
+        choice=int(input("Please choose what would you like to count(1-7): "))
+        if choice==7:
+            break
+        elif choice>7 or choice<1:
+            print("Please choose between 1-7")
+            continue
+        else:
+            number1=float(input("Please enter the 1. number: "))
+            number2=float(input("Please enter the 2.number: "))
+            if choice==1:
+                print(addition(number1,number2))
+            elif choice==2:
+                print(substraction(number1,number2))
+            elif choice==3:
+                print(multiplication(number1,number2))
+            elif choice==4:
+                print(division(number1,number2))
+            elif choice==5:
+                print(power(number1,number2))
+            elif choice==6:
+                print(root(number1,number2))
+except ValueError:
+    print("Wrong input, you can choose only between 1-7. Please start over")
+
+print("Thank you for using the calculator")
